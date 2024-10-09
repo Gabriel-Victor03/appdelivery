fe0eaee0fdaf682567c4e2b99627bee646847296
Gabriel Victor <gabrielvictorman@gmail.com>
2024-10-09 19:52:10 -0300

    tirando const

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -5,7 +5,7 @@ import 'package:appdelivery/view/components/my_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
