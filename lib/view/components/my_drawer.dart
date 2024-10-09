1edb792f69876f36e2b9372c6d89ff73ae4061e6
Gabriel Victor <gabrielvictorman@gmail.com>
2024-10-09 19:30:11 -0300

    mudanças no drwaer

--- lib/view/components/my_drawer.dart
+++ lib/view/components/my_drawer.dart
@@ -5,48 +5,64 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer (
-      backgroundColor: Colors.white,
-          child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Container(
-              //     // alignment: Alignment(-1.0, -2.5),
-              //     ),
-              Text(
-                'Administração',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Color.fromARGB(255, 130, 30, 60),
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Arial',
-                ),
+    return Drawer(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Text(
+              'Menu',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              Text('Login',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                    // fontWeight: FontWeight.bold,
-                    fontFamily: 'Arial',
-                  )),
-              Text(
-                'Endereço',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Color.fromARGB(255, 130, 30, 60),
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Arial',
-                ),
-              ),
-              Text('GO-154, km 218 - Zona Rural, Ceres - GO, 76300-000',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                    // fontWeight: FontWeight.bold,
-                    fontFamily: 'Arial',
-                  ))
-            ],
+            ),
           ),
+          ListTile(
+            title: Text(
+              'Administração',
+              style: TextStyle(
+                fontSize: 20,
+                color: Color.fromARGB(255, 130, 30, 60),
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Arial',
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.black,
+                fontFamily: 'Arial',
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Endereço',
+              style: TextStyle(
+                fontSize: 20,
+                color: Color.fromARGB(255, 130, 30, 60),
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Arial',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'GO-154, km 218 - Zona Rural, Ceres - GO, 76300-000',
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.black,
+                fontFamily: 'Arial',
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
