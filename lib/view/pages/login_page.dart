fe4cb2c8dfe3547f26d316be61c16b7e3354fe89
Adauto Turíbio <adautoturibio73@gmail.com>
2024-10-09 19:41:07 -0300

    tirando os const

--- lib/view/pages/login_page.dart
+++ lib/view/pages/login_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MeuApp extends StatefulWidget {
-  const MeuApp({super.key});
+  MeuApp({super.key});
 
   @override
   State<MeuApp> createState() => _MeuAppState();
@@ -25,69 +26,65 @@ class _MeuAppState extends State<MeuApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 130, 30, 60),
-          foregroundColor: Colors.white,
-        ),
-        endDrawer: const Drawer(
-          backgroundColor:
-              Color.fromARGB(255, 255, 229, 184), //Podemos mudar a cor de fundo
-          // child: DrawerHeader(
-          //     // decoration: FlutterLogoDecoration(textColor: Colors.white)
-          //     //podemos adicionar o cabeçalho como um 'filho' do Drawer
-          //     ),
-        ),
-        body: Center(
-          child: Container( 
-            height: 350,
-            width: 400,
-            decoration: BoxDecoration(
-              //Podemos utilizar a tag decoration para mudarmos a aparência do container
-              color: Color.fromARGB(255, 255, 229, 184),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  const Text(
-                    'LOGIN',  // Texto adicionado no começo
-                    style: TextStyle(
-                      fontSize: 18, 
-                      fontWeight: FontWeight.bold
-                     ),
-                    ),
-                  SizedBox(height: 20),  // Espaçamento entre o texto e o TextField
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 130, 30, 60),
+            foregroundColor: Colors.white,
+          ),
+          endDrawer: Drawer(
+            backgroundColor: Color.fromARGB(
+                255, 255, 229, 184), //Podemos mudar a cor de fundo
+            // child: DrawerHeader(
+            //     // decoration: FlutterLogoDecoration(textColor: Colors.white)
+            //     //podemos adicionar o cabeçalho como um 'filho' do Drawer
+            //     ),
+          ),
+          body: Center(
+            child: Container(
+              height: 350,
+              width: 400,
+              decoration: BoxDecoration(
+                //Podemos utilizar a tag decoration para mudarmos a aparência do container
+                color: Color.fromARGB(255, 255, 229, 184),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(children: [
+                  Text(
+                    'LOGIN', // Texto adicionado no começo
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                      height: 20), // Espaçamento entre o texto e o TextField
 
-                  const Text(
-                    'Nome:',  // Título acima do primeiro TextField
+                  Text(
+                    'Nome:', // Título acima do primeiro TextField
                     style: TextStyle(fontSize: 16),
                   ),
-                  const TextField(
+                  TextField(
                     decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: "E-mail",
                         filled: true,
                         fillColor: Colors.white),
                   ),
-                  SizedBox(height: 40),  // Espaçamento entre o texto e o TextField
-                  const TextField(
+                  SizedBox(
+                      height: 40), // Espaçamento entre o texto e o TextField
+                  TextField(
                     decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: "Senha",
                         filled: true,
                         fillColor: Colors.white),
                   ),
-                  SizedBox(height: 40),  // Espaçamento entre o texto e o TextField
-                  ElevatedButton(
-                    onPressed: () {  }, 
-                    child: const Text("Pressione")),
-              ]),
+                  SizedBox(
+                      height: 40), // Espaçamento entre o texto e o TextField
+                  ElevatedButton(onPressed: () {}, child: Text("Pressione")),
+                ]),
+              ),
             ),
-          ),
-        )
-      ),
+          )),
     );
   }
 }
