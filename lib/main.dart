ddebaa922735fc3c169a2da66de5178a4c25ba8a
Tulio-Eloi <tulioeloi29@gmail.com>
2024-10-24 19:28:50 -0300

    Ajuste do BAnco

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
 import 'package:appdelivery/view/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:appdelivery/view/pages/home_page.dart';
-import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
+// import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   const keyApplicationId = 'XWN5AL2fKOQXxKnSnAdeLGHvx68NakUwPMhHTk1B';
   const keyClientKey = 'x2iLiWANqMLd1MRnotpzyyxLoJEJrTuVd2HkS3K1';
   const keyParseServerUrl = 'https://parseapi.back4app.com';
 
-  await Parse().initialize(
+  /*await Parse().initialize(
     keyApplicationId, 
     keyParseServerUrl,
     clientKey: keyClientKey, 
     debug: true
-  );
+  );*/
 
-   var firstObject = ParseObject('FirstClass')
-    ..set('message', 'Hey, Parse is now connected!🙂');
-  await firstObject.save();
+   // var firstObject = ParseObject('FirstClass')
+  //  ..set('message', 'Hey, Parse is now connected!🙂');
+  // await firstObject.save();
 
   print("done");
   
-  WidgetsFlutterBinding.ensureInitialized();
+  
   runApp(MeuApp());
 }
 
