5186d38c54016c8c82ddac24aa704e6d45ad8fdb
Ericfg05 <ericfg05@gmail.com>
2024-10-28 17:27:35 -0400

    continuação da tela de produtos

--- lib/view/pages/product_page.dart
+++ lib/view/pages/product_page.dart
@@ -27,60 +27,94 @@ class _ProductPageState extends State<ProductPage> {
             ),
           ),
         ),
-        body: Column(
-          children: [
-            //Center(child: MyTable()),
-            SizedBox(
-              height: 10,
-            ),
-            Center(
-                child: SizedBox(
-                    width: 200,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            elevation: 2.0,
-                            backgroundColor:
-                                const Color.fromARGB(255, 0, 26, 255),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(10),
+        body: IntrinsicHeight(
+          child: Column(
+            children: [
+              //Center(child: MyTable()),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                  child: SizedBox(
+                      width: 200,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              elevation: 2.0,
+                              backgroundColor:
+                                  const Color.fromARGB(255, 0, 26, 255),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
                               ),
-                            ),
-                            side:
-                                BorderSide(color: Color.fromARGB(50, 0, 0, 0))),
-                        onPressed: () {
-                          openDialog();
-                        },
-                        child: Row(
-                          children: [
-                            Positioned(
-                                right: 110,
-                                child: Container(
-                                    child: Icon(
-                                  Icons.edit,
-                                  color:
-                                      const Color.fromARGB(255, 255, 255, 255),
-                                ))),
-                            Positioned(
-                                child: Text(
-                              "Editar Categorias",
-                              style: TextStyle(
-                                  color:
-                                      const Color.fromARGB(255, 255, 255, 255)),
-                            ))
-                          ],
-                        )))),
-          ],
+                              side: BorderSide(
+                                  color: Color.fromARGB(50, 0, 0, 0))),
+                          onPressed: () {
+                            openDialog();
+                          },
+                          child: Row(
+                            children: [
+                              Positioned(
+                                  right: 110,
+                                  child: Container(
+                                      child: Icon(
+                                    Icons.edit,
+                                    color: const Color.fromARGB(
+                                        255, 255, 255, 255),
+                                  ))),
+                              Positioned(
+                                  child: Text(
+                                "Editar Categorias",
+                                style: TextStyle(
+                                    color: const Color.fromARGB(
+                                        255, 255, 255, 255)),
+                              ))
+                            ],
+                          )))),
+            ],
+          ),
         ),
       );
   Future openDialog() => showDialog(
       context: context,
       builder: (context) => AlertDialog(
-            title: Center(
-              child: Text(
-                "Editar Categorias",
-                style: TextStyle(fontWeight: FontWeight.w700),
-              ),
+            title: Stack(
+              children: [
+                for (var i = 0; i < 5; i++)
+                  SizedBox(
+                    height: 200,
+                    child: Divider(
+                      color: Colors.black,
+                    ),
+                  ),
+                Positioned(
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        "Editar Categorias",
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                    top: 10,
+                    child: Container(
+                        child: Divider(
+                      color: Colors.black,
+                      indent: 10,
+                      endIndent: 10,
+                    ))),
+                Positioned(
+                    top: 60,
+                    child: Container(
+                      child: Text(
+                        "Categorias",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: 16),
+                      ),
+                    ))
+              ],
             ),
             content: TextField(
               autofocus: true,
