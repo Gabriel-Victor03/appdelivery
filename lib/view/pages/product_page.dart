964188c79a90bfb52f78fcf66e1a4bdec51f4975
Ericfg05 <ericfg05@gmail.com>
2024-10-31 15:25:16 -0400

    Melhorando o pop-up

--- lib/view/pages/product_page.dart
+++ lib/view/pages/product_page.dart
@@ -1,9 +1,6 @@
-import 'dart:js_interop';
-
-import 'package:appdelivery/view/components/my_table.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
 void main() {
@@ -80,87 +77,303 @@ class _ProductPageState extends State<ProductPage> {
       context: context,
       builder: (context) => Dialog(
             child: Container(
-              width: 400,
-              height: 300,
-              child: Stack(
-                children: [
-                  Positioned(
-                      left: 125,
-                      top: 10,
-                      child: Text(
-                        'Editar Categoria',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 22),
-                      )),
-                  Positioned(
-                      //top: 5,
-                      left: 25,
-                      top: 40,
-                      width: 350,
-                      child: Divider(
-                        color: Colors.black,
-                        indent: 1,
-                      )),
-                  Positioned(
-                      left: 45,
-                      top: 180,
-                      child: Text(
-                        "Nova Categoria",
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      )),
-                  Wrap(
-                    // mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Positioned(
-                        top: 80,
-                        child: Container(
-                          margin: EdgeInsets.all(10),
-                          child: Text(
-                            "Hambúrguer",
-                          ),
+                width: 400,
+                height: 400,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: Center(
+                        child: Text(
+                          "Editar Categoria",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
                         ),
-                      ),
-                      Positioned(
-                        top: 80,
-                        child: Container(
-                          margin: EdgeInsets.all(10),
-                          child: Text(
-                            "Hambúrguer",
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Positioned(
-                    left: 45,
-                    top: 200,
-                    width: 150,
-                    child: TextField(
-                      cursorColor: const Color.fromARGB(255, 0, 0, 0),
-                      autofocus: false,
-                      decoration: InputDecoration(
-                        fillColor: Color.fromARGB(255, 0, 150, 255),
-                        // icon: Icon(Icons.import_contacts),
                       ),
                     ),
-                  ),
-                  Positioned(
-                      left: 220,
-                      top: 220,
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.green,
-                            //shape: Border(bottom: 10, top: 10, left: 20, right: 20)
+                    Divider(
+                      //divider que fica abaixo do Editar categoria.
+                      indent: 10.0,
+                      endIndent: 10.0,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(0, 20, 250, 20),
+                      child: Text(
+                        "Categorias",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 150,
+                      child: SingleChildScrollView(
+                        child: Wrap(
+                          //scrollDirection: Axis.vertical,
+                          // alignment: WrapAlignment.start,
+                          children: [
+                            Row(children: [
+                              Container(
+                                padding: EdgeInsets.only(left: 30),
+                                child: Text(
+                                  "Hambúrguer",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.normal),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.topRight,
+                                child: Container(
+                                  padding: EdgeInsets.only(left: 190),
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      shape: CircleBorder(),
+                                      backgroundColor: Colors.red,
+                                    ),
+                                    child: Icon(
+                                      Icons.delete,
+                                      color: Colors.white,
+                                      weight: 4,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ]),
+                            Container(
+                              padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                              child: Divider(
+                                indent: 1.0,
+                                endIndent: 1.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 30),
+                              child: Text(
+                                "Bebidas",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 190),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  shape: CircleBorder(),
+                                  backgroundColor: Colors.red,
+                                ),
+                                child: Icon(
+                                  Icons.delete,
+                                  color: Colors.white,
+                                  weight: 4,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                              child: Divider(
+                                indent: 1.0,
+                                endIndent: 1.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 30),
+                              child: Text(
+                                "Sobremessas",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 190),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  shape: CircleBorder(),
+                                  backgroundColor: Colors.red,
+                                ),
+                                child: Icon(
+                                  Icons.delete,
+                                  color: Colors.white,
+                                  weight: 4,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                              child: Divider(
+                                indent: 1.0,
+                                endIndent: 1.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 30),
+                              child: Text(
+                                "Combos",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 190),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  shape: CircleBorder(),
+                                  backgroundColor: Colors.red,
+                                ),
+                                child: Icon(
+                                  Icons.delete,
+                                  color: Colors.white,
+                                  weight: 4,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                              child: Divider(
+                                indent: 1.0,
+                                endIndent: 1.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 30),
+                              child: Text(
+                                "Jatinhas",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 190),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  shape: CircleBorder(),
+                                  backgroundColor: Colors.red,
+                                ),
+                                child: Icon(
+                                  Icons.delete,
+                                  color: Colors.white,
+                                  weight: 4,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                              child: Divider(
+                                indent: 1.0,
+                                endIndent: 1.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Row(children: [
+                              Container(
+                                padding: EdgeInsets.only(left: 30),
+                                child: Text(
+                                  "sucos",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.normal),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Container(
+                                  padding: EdgeInsets.only(left: 190),
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      shape: CircleBorder(),
+                                      backgroundColor: Colors.red,
+                                    ),
+                                    child: Icon(
+                                      Icons.delete,
+                                      color: Colors.white,
+                                      weight: 4,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ]),
+                            Container(
+                              padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                              child: Divider(
+                                indent: 1.0,
+                                endIndent: 1.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Text("Nova Categoria"),
+                    ),
+                    Positioned(
+                      // top: 50,
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 80,
                           ),
-                          onPressed: () {},
-                          child: Icon(
-                            Icons.check,
-                            color: const Color.fromARGB(255, 255, 255, 255),
-                          )))
-                ],
-              ),
-            ),
+                          Container(
+                            width: 200,
+                            height: 30,
+                            padding: EdgeInsets.only(left: 15),
+                            child: TextField(
+                              cursorWidth: 1,
+                              cursorHeight: 20,
+                              textAlign: TextAlign.center,
+                              cursorColor: const Color.fromARGB(255, 0, 0, 0),
+                              //autofocus: false,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 0, vertical: 0),
+
+                                // icon: Icon(Icons.import_contacts),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            //decoration: BoxDecoration(color: Colors.green),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.green,
+                                  iconColor: Colors.white),
+                              onPressed: () {},
+                              child: Center(
+                                child: Icon(Icons.check,
+                                    color: Colors.white, size: 20),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                            child: Divider(
+                              indent: 1.0,
+                              endIndent: 1.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
             //Positioned()
           ));
 }
