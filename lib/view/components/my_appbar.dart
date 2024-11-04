ccc71c3db8c58cc01f67810bafc9282d6ee4d1da
Gabriel Victor <gabrielvictorman@gmail.com>
2024-11-03 21:36:41 -0300

    centralizando logo na appBar

--- lib/view/components/my_appbar.dart
+++ lib/view/components/my_appbar.dart
@@ -8,15 +8,21 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       backgroundColor: const Color.fromARGB(255, 130, 30, 60),
       foregroundColor: Colors.white,
-      leading: GestureDetector(
-        onTap: () {
-          Navigator.pushNamed(context, '/homepage'); // Navega para a HomePage
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image.asset('assets/images/logo.png'),
+      title: Center(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, '/homepage'); // Navega para a HomePage
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              'assets/images/logo.png',
+              height: 35,
+            ),
+          ),
         ),
       ),
+      toolbarHeight: 60,
     );
   }
 
