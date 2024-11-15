42d175b8e86487bbf3efe6793350f500c20700f8
Gabriel Victor <gabrielvictorman@gmail.com>
2024-11-15 11:21:22 -0300

    tela sacola e popup de endereço finalizado

--- lib/view/components/my_popup_informaddress.dart
+++ lib/view/components/my_popup_informaddress.dart
@@ -12,6 +12,8 @@ class _MyPopupInformAddress extends State<MyPopupInformAddress> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      backgroundColor: const Color.fromARGB(255, 255, 229, 184),
+
       title: Center(
           child: Column(children: [
         const Text("Endereço de Entrega"),
@@ -23,76 +25,247 @@ class _MyPopupInformAddress extends State<MyPopupInformAddress> {
       ])),
       titleTextStyle: TextStyle(
           fontFamily: 'Arial',
-          fontSize: 20,
+          fontSize: 18,
           fontWeight: FontWeight.bold,
           color: Colors.black),
 
       //
 
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Rua',
-              border: OutlineInputBorder(),
+      content: Container(
+        padding: const EdgeInsets.all(02),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Rua',
+                        style: TextStyle(
+                          fontFamily: 'arial',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(
+                        height: 40,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Colors.white,
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 05,
+                              horizontal: 10,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Número',
+                        style: TextStyle(
+                          fontFamily: 'arial',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(
+                        height: 40,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Colors.white,
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 05,
+                              horizontal: 10,
+                            ),
+                          ),
+                          keyboardType: TextInputType.number,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ),
-          const SizedBox(height: 10),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Número',
-              border: OutlineInputBorder(),
+            const SizedBox(height: 10),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Bairro',
+                        style: TextStyle(
+                          fontFamily: 'arial',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(
+                        height: 40,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Colors.white,
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 05,
+                              horizontal: 10,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Cidade',
+                        style: TextStyle(
+                          fontFamily: 'arial',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Container(
+                        height: 40,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Colors.white,
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 05,
+                              horizontal: 10,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-            keyboardType: TextInputType.number,
-          ),
-          const SizedBox(height: 10),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Bairro',
-              border: OutlineInputBorder(),
+            const SizedBox(height: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Complemento (opcional)',
+                  style: TextStyle(
+                    fontFamily: 'arial',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Container(
+                  height: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 05,
+                        horizontal: 10,
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
-          ),
-          const SizedBox(height: 10),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Cidade',
-              border: OutlineInputBorder(),
+            const SizedBox(height: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Ponto de Referência',
+                  style: TextStyle(
+                    fontFamily: 'arial',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Container(
+                  height: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 05,
+                        horizontal: 10,
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
-          ),
-          const SizedBox(height: 10),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Complemento (opcional)',
-              border: OutlineInputBorder(),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
+
+      //
+
       actions: [
         // TextButton(
         //   onPressed: () => Navigator.of(context).pop(),
         //   child: const Text("Cancelar"),
         // ),
-        ElevatedButton(
-          onPressed: () {
-            // Lógica para salvar o endereço
-            Navigator.of(context).pop();
-          },
-          child: const Text("Salvar"),
-          style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.green, // Cor do texto branca
-            textStyle: const TextStyle(
-              fontFamily: 'Arial', // Fonte Arial
-              fontWeight: FontWeight.bold, // Negrito
-            ),
-            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
+        Container(
+          child: ElevatedButton(
+            onPressed: () {
+              // Lógica para salvar o endereço
+              Navigator.of(context).pop();
+            },
+            child: const Text("Salvar"),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: const Color.fromARGB(255, 53, 155, 56),
+              textStyle: const TextStyle(
+                fontFamily: 'Arial',
+                fontWeight: FontWeight.bold,
+              ),
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
             ),
           ),
-        ),
+        )
       ],
     );
   }
