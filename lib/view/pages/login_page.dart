cbfc831daa3c9a940bb2da4b530370e338a5a825
Tulio-Eloi <tulioeloi29@gmail.com>
2024-11-21 20:02:52 -0300

    Ajuste tela de Login

--- lib/view/pages/login_page.dart
+++ lib/view/pages/login_page.dart
@@ -4,16 +4,33 @@ import 'package:appdelivery/view/components/my_drawer.dart';
 import 'package:appdelivery/view/components/my_appbar.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  const keyApplicationId = 'XWN5AL2fKOQXxKnSnAdeLGHvx68NakUwPMhHTk1B';
+  const keyClientKey = 'x2iLiWANqMLd1MRnotpzyyxLoJEJrTuVd2HkS3K1';
+  const keyParseServerUrl = 'https://parseapi.back4app.com';
+
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, debug: true);
+
+  var firstObject = ParseObject('FirstClass')..set('message', 'teste');
+  await firstObject.save();
+
+  print("done");
+}
 class LoginPage extends StatefulWidget {
   LoginPage({super.key});
-  final TextEditingController controllerUsername = TextEditingController();
-  final TextEditingController controllerPassword = TextEditingController();
 
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController controllerUsername = TextEditingController();
+  final TextEditingController controllerPassword = TextEditingController();
+  bool isLoggedIn = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,21 +47,29 @@ class _LoginPageState extends State<LoginPage> {
             padding: EdgeInsets.all(8.0),
             child: Column(children: [
               TextField(
-                controller: widget.controllerUsername, // Acessando o controlador corretamente
+                controller: controllerUsername, // Acessando o controlador corretamente
+                enabled: !isLoggedIn,
+                keyboardType: TextInputType.text,
+                textCapitalization: TextCapitalization.none,
+                autocorrect: false,
                 decoration: InputDecoration(labelText: 'Username'),
               ),
               TextField(
-                controller: widget.controllerPassword, // Acessando o controlador corretamente
+                controller: controllerPassword, // Acessando o controlador corretamente
+                enabled: !isLoggedIn,
+                keyboardType: TextInputType.text,
+                textCapitalization: TextCapitalization.none,
+                autocorrect: false,
                 decoration: InputDecoration(labelText: 'Password'),
                 obscureText: true,
               ),
               ElevatedButton(
-                 onPressed: () {Navigator.pushReplacementNamed(context, '/painel_adm');},//=> doUserLogin(), // Chamando a função de login
+                onPressed: isLoggedIn ? null : () => doUserLogin(), // Chamando a função de login
                 child: Text('Login'),
               ),
               TextButton(
                 onPressed: () => continueWithoutLogin(context),
-                child: Text('Continuar sem login'),
+                child: Text('Continuar sem fazer login'),
               ),
             ]),
           ),
@@ -53,9 +78,49 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
+void showSuccess(String message) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text("Success!"),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: const Text("OK"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+void showError(String errorMessage) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text("Error!"),
+        content: Text(errorMessage),
+        actions: <Widget>[
+          TextButton(
+            child: const Text("OK"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
   void doUserLogin() async {
-    final username = widget.controllerUsername.text.trim(); // Acessando o controlador corretamente
-    final password = widget.controllerPassword.text.trim(); // Acessando o controlador corretamente
+    final username = controllerUsername.text.trim(); // Acessando o controlador corretamente
+    final password = controllerPassword.text.trim(); // Acessando o controlador corretamente
 
     final user = ParseUser (username, password, null);
 
@@ -63,8 +128,7 @@ class _LoginPageState extends State<LoginPage> {
 
     if (response.success) {
       // Redirecionar para o painel ADM
-      // Navigator.pushReplacementNamed(context, '/painel_adm');
-      print("sucesso");
+      Navigator.pushReplacementNamed(context, '/painel_adm');
     } else {
       // Exibir mensagem de erro
       print(response.error?.message ?? 'Erro desconhecido');
